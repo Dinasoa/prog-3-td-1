fdb8c8d2117d370a20e12600ad37afee88761f2f
dinasoa <hei.dinasoa@gmail.com>
2022-12-12 11:12:35 +0300

    to-do3: use int over Integer

--- src/main/java/app/prog/service/BookService.java
+++ src/main/java/app/prog/service/BookService.java
@@ -30,9 +30,9 @@ public class BookService {
     }
 
     //TODO-3: should I use Integer here or int ? Why ?
+    public BookEntity deleteBook(Integer id) {
     //We should use int over Integer because bookEntityID is not nullable
 
-    public BookEntity deleteBook(Integer id) {
         /*
         TIPS: From the API, the Class Optional<T> is :
         A container object which may or may not contain a non-null value.
